274c97f26855d32d4030c7bd948c5b0c5a982cf9
JCSR <johnconnorsryan@gmail.com>
2020-04-30 21:23:56 -0400

    connected fetch requests to html submit button and text area

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,17 +1,12 @@
-java
-// IMDB: k._.x.G.W.s.0.T.0.8. REMOVE PERIODS
 
-var userInput = document.querySelector('.test1')
-var secondInput = document.querySelector('.test2')
-var thirdInput = document.querySelector('.test3')
-
-var titletest1El = document.querySelector('.title')
-var scoretest1El = document.querySelector('.ratings')
-var imgtest1El = document.querySelector('.image')
-var directortestEl = document.querySelector('.director')
+userInputEl = document.querySelector('.userInput')
+titleEl= document.querySelector('.title')
+posterEl= document.querySelector('.poster')
+ratingEl= document.querySelector('.ratings')
+directorEl= document.querySelector('.director')
 
 function test1() {
-    fetch('https://www.omdbapi.com/?apikey=2acf9b30&t='  + userInput.value )
+    fetch('https://www.omdbapi.com/?apikey=2acf9b30&t='  + userInputEl.value )
     .then(response => response.json())
     .then(data => {
         console.log(data)
@@ -19,15 +14,15 @@ function test1() {
         var score = data['Ratings'][1]['Value'];
         var poster = data['Poster']
 
-        titletest1El.innerHTML = title;
-        scoretest1El.innerHTML = 'Rotten Tomatoes: ' + score;
-        imgtest1El.setAttribute('src', poster)
+        titleEl.innerHTML = title;
+        ratingEl.innerHTML = 'Rotten Tomatoes: ' + score;
+        posterEl.setAttribute('src', poster)
     }) 
 };
 
 function test2() {
-    let term = secondInput.value;
-    fetch('https://content.guardianapis.com/search?q=' + secondInput.value + '&api-key=4298cb73-e3b3-4b9c-8183-aeceb09d2290')
+    
+    fetch('https://content.guardianapis.com/search?q=' + userInputEl.value + '&api-key=4298cb73-e3b3-4b9c-8183-aeceb09d2290')
     .then(response => response.json())
     .then(data =>
         console.log(data))
@@ -35,13 +30,12 @@ function test2() {
 
 
 function test3() {
-    let term = thirdInput.value;
-    fetch('https://itunes.apple.com/search?term=' + term + '&entity=movie')
+    fetch('https://itunes.apple.com/search?term=' + userInputEl.value + '&entity=movie')
     
     .then(response => response.json())
     .then(data => {
         console.log(data)
         var director =data['results'][0]['artistName']
-        directortestEl.innerHTML = director;
+        directorEl.innerHTML = director;
     });
     };
